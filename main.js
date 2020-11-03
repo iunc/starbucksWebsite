d6847adb6e1312386cb15df9eebe8d64b8963d30
DangKhoa <iunc129@gmail.com>
2020-11-04 00:33:51 +0700

    add

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgSlider(srcimg) {
+    document.querySelector('.starbucks').src = srcimg ;
+}
+function changCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
